315e1d1f769f136582cead55c4a3e35fa3638594
wahyurhy <wahyurahayu82@gmail.com>
2022-11-14 19:55:37 +0700

    init

---
+++ app/src/main/java/com/wahyurhy/moviappandroid/presentation/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wahyurhy.moviappandroid.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wahyurhy.moviappandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/wahyurhy/moviappandroid/presentation/view/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.wahyurhy.moviappandroid.presentation.view.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wahyurhy.moviappandroid.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
